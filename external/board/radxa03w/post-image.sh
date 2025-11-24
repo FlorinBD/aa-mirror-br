8b95e50799af393c86660eb47c3ef6a1d58debd7
FlorinBD <FlorinBD@users.noreply.github.com>
2025-11-24 16:58:13 +0200

    Update post-image.sh

--- external/board/radxa03w/post-image.sh
+++ external/board/radxa03w/post-image.sh
@@ -4,5 +4,5 @@ set -u
 set -e
 set -x
 
-support/scripts/genimage.sh -c $BR2_EXTERNAL_AA_PROXY_OS_PATH/board/radxa03w/genimage.cfg
+support/scripts/genimage.sh -c $BR2_EXTERNAL_AA_MIRROR_OS_PATH/board/radxa03w/genimage.cfg
 
