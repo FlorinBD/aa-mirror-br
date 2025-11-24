adb38c4a2da56015b8863ba02b81ce2b1587cc13
FlorinBD <FlorinBD@users.noreply.github.com>
2025-11-24 16:57:36 +0200

    Update post-build.sh

--- external/board/common/post-build.sh
+++ external/board/common/post-build.sh
@@ -3,8 +3,8 @@
 set -u
 set -e
 
-${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/common/generate-issue.sh
+${BR2_EXTERNAL_AA_MIRROR_OS_PATH}/board/common/generate-issue.sh
 
-source ${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/common/add_usb_serial.sh
+source ${BR2_EXTERNAL_AA_MIRROR_OS_PATH}/board/common/add_usb_serial.sh
 
 
