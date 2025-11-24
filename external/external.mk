85d481a6f603394f123082c7475f695a2f44315f
FlorinBD <FlorinBD@users.noreply.github.com>
2025-11-24 16:11:19 +0200

    Update external.mk

--- external/external.mk
+++ external/external.mk
@@ -1,2 +1,2 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_AA_PROXY_OS_PATH)/package/*/*.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_AA_PROXY_OS_PATH)/boot/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_AA_MIRROR_OS_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_AA_MIRROR_OS_PATH)/boot/*/*.mk))
