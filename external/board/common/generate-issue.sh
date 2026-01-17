784b641476e8b3b72bb8525aca46e86d8e3b0a7b
FlorinBD <FlorinBD@users.noreply.github.com>
2026-01-17 14:35:15 +0200

    Update generate-issue.sh

--- external/board/common/generate-issue.sh
+++ external/board/common/generate-issue.sh
@@ -6,12 +6,13 @@ BUILDROOT_COMMIT=$(git -C "$BUILDROOT_DIR" log -n1 --pretty=format:%h HEAD)
 
 cat <<EOF > "$TARGET_DIR/etc/issue"
 Welcome to aa-proxy
-
+                                                                 
                              _
   __ _  __ _       _ __ ___ (_)_ __ _ __ ___  _ __      _ __ ___ 
  / _` |/ _` |_____| '_ ` _ \| | '__| '__/ _ \| '__|____| '__/ __|
 | (_| | (_| |_____| | | | | | | |  | | | (_) | | |_____| |  \__ \
  \__,_|\__,_|     |_| |_| |_|_|_|  |_|  \___/|_|       |_|  |___/
+                                                                  
 
 https://github.com/aa-proxy/aa-proxy-rs
 build date: $(date +%Y-%m-%d), br# $BUILDROOT_COMMIT
